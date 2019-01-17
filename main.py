a8a2a40805b4e7ed77b8a85ba1020ada11ee5bc4
Nicolas Pons <nicolas.pons96@gmail.com>
2019-01-17 12:47:34 +0100

    main.py: works with colored picture

--- main.py
+++ main.py
@@ -6,7 +6,21 @@ sharpen = [0, -1, 0, -1, 5, -1, 0, -1, 0]
                 
 if __name__ == "__main__":
     im = Image.open("ressources/orig.png")
-    new = im.convert('LA')
+    im.show()
+    listim = list(im.getdata())
+    listr, listg, listb = zip(*listim)
+
+    kernel = sharpen
+
+    listr = convolution(listr, kernel)
+    listg = convolution(listg, kernel)
+    listb = convolution(listb, kernel)
+
+    listim = list(zip(listr, listg, listb))
+    im2 = Image.new(im.mode, im.size)
+    im2.putdata(listim)
+    im2.show()
+    """
     vec = list(new.getdata())
     new.show()
     l = [x[0] for x in vec] 
@@ -15,3 +29,4 @@ if __name__ == "__main__":
     im2 = Image.new(im.mode, im.size)
     im2.putdata(newl)
     im2.show()
+    """
